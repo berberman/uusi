d62cbf9c73c576b849ad56dda7394a59a4141e9a
Potato Hatsue <1793913507@qq.com>
2020-11-13 15:40:36 +0800

    Fix duplicate dependencies caused by replace, refactor cli

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Main (main) where
 
 import Control.Monad (unless, when)
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Distribution.PackageDescription.Parsec (readGenericPackageDescription)
@@ -27,68 +30,106 @@ import System.Environment (getArgs)
 main :: IO ()
 main = do
   args <- getArgs
-  (AOption actions, target) <- runOption args
+  (joinOptions -> actions, target) <- runOption args
   let useDefaultAction = null actions
       actions' = if useDefaultAction then [allToAnyVersion] else actions
 
   when useDefaultAction $
-    T.putStrLn "You didn't pass -a, remove all version constraints of dependencies."
+    T.putStrLn "You didn't pass any option, use -all."
 
   unless useDefaultAction $ do
     T.putStrLn "Pending action(s):"
     ("  " <>) . show <$> actions' |> unlines |> init |> putStrLn
   uusiCabal actions' target
 
-runOption :: [String] -> IO (AOption, FilePath)
-runOption argv = case getOpt Permute [option] argv of
-  (o, [n], []) -> return (chain o <| defaultOption, n)
-  (_, _, err) -> ioError <| userError <| concat err <> usageInfo help [option]
+runOption :: [String] -> IO (Options, FilePath)
+runOption argv = case getOpt Permute cliOptions argv of
+  (o, [n], []) -> return (chain o <| defaultOptions, n)
+  (_, _, err) -> ioError <| userError <| concat err <> usageInfo help cliOptions
   where
-    help = "uusi - tweak .cabal file (replace inplace)"
+    help = "uusi - tweak .cabal file (replace inplace) | useage: uusi [OPTIONS] TARGET"
 
-newtype AOption = AOption
-  { optActions :: SomeUusi
+-----------------------------------------------------------------------------
+
+data Options = Options
+  { optAll :: Bool,
+    optOverwrite :: SomeUusi,
+    optRemove :: SomeUusi,
+    optReplace :: SomeUusi
   }
   deriving stock (Show)
 
-defaultOption :: AOption
-defaultOption = AOption []
+defaultOptions :: Options
+defaultOptions = Options False [] [] []
 
-option :: OptDescr (AOption -> AOption)
-option =
-  Option
-    ['a']
-    ["action"]
-    ( OptArg
-        ( \arg opt -> case arg >>= parseAction of
-            Just action -> opt {optActions = action : optActions opt}
-            _ -> opt
-        )
-        "ACTION"
-    )
-    <| unlines
-      [ "There are three kinds of actions:",
-        "PACKAGE:remove        -- remove PACKAGE from dependencies              | e.g. base:remove",
-        "PACKAGE:VERSION_RANGE -- overwrite the version range of PACKAGE        | e.g. base: >= 4.14 && < 4.15 ",
-        "...                                                                    | e.g. text:-any",
-        "all                   -- remove all version constraints of dependencies",
-        "",
-        "They can be used together: uusi -aACTION1 -aACTION2 ... TARGET",
-        "default: all"
-      ]
+joinOptions :: Options -> SomeUusi
+joinOptions Options {..} = [allToAnyVersion | optAll] <> optOverwrite <> optRemove <> optReplace
 
-parseAction :: String -> Maybe Uusi
-parseAction s = case T.splitOn ":" (T.pack s) of
-  [T.unpack . T.strip -> name, t] -> case t of
-    "remove" -> name |> mkPackageName |> removeByName |> Just
-    _ -> case t |> T.unpack |> simpleParsec of
-      Just ver -> overwriteByName (name |> mkPackageName) ver |> Just
-      _ -> Nothing
-  ["all"] -> Just allToAnyVersion
+cliOptions :: [OptDescr (Options -> Options)]
+cliOptions =
+  [ Option
+      []
+      ["all"]
+      (NoArg (\opts -> opts {optAll = True}))
+      "remove all version constraints | e.g. -all",
+    Option
+      ['u']
+      ["update"]
+      ( ReqArg
+          ( \arg opts -> case parsePkg arg of
+              Just action -> opts {optOverwrite = (action |> uncurry overwriteByName) : optOverwrite opts}
+              _ -> error $ "faild to parse overwrite: " <> arg
+          )
+          "PACKAGE(:VERSION_RANGE)"
+      )
+      "overwrite the version range of PACKAGE | e.g. -ubase: >= 4.14 && < 4.15",
+    Option
+      ['d']
+      ["delete"]
+      ( ReqArg
+          ( \arg opts -> case simpleParsec arg of
+              Just name -> opts {optRemove = removeByName name : optRemove opts}
+              _ -> error $ "faild to parse remove: " <> arg
+          )
+          "PACKAGE"
+      )
+      "remove PACKAGE | e.g. -dbase",
+    Option
+      ['r']
+      ["replace"]
+      ( ReqArg
+          ( \arg opts -> case parseReplace arg of
+              Just action -> opts {optReplace = action : optReplace opts}
+              _ -> error $ "faild to parse replace: " <> arg
+          )
+          "SOURCE_PACKAGE:DEST1_PACKAGE(:DEST1_VERSION),DEST2_PACKAGE(:DEST2_VERSION)..."
+      )
+      "replace PACKAGE with a set of packages | e.g. -rbase:text,bytestring"
+  ]
+
+parsePkg :: String -> Maybe (PackageName, VersionRange)
+parsePkg s = case T.splitOn ":" (T.pack s) of
+  [name, v] -> case v |> T.unpack |> simpleParsec of
+    Just ver -> (,ver) <$> (name |> T.unpack |> simpleParsec)
+    _ -> Nothing
+  [name] -> (,anyVersion) <$> (name |> T.unpack |> simpleParsec)
   _ -> Nothing
 
+parseReplace :: String -> Maybe Uusi
+parseReplace (T.pack -> s)
+  | (Just source) <- s |> T.takeWhile (/= ':') |> T.unpack |> simpleParsec,
+    k <- s |> T.dropWhile (/= ':'),
+    not <| T.null k,
+    rest <- k |> T.tail |> T.splitOn ",",
+    action <- [r | x <- rest, r <- catMaybes [x |> T.unpack |> parsePkg]],
+    not <| null action =
+    action |> replaceByName source |> Just
+  | otherwise = Nothing
+
+-----------------------------------------------------------------------------
+
 allToAnyVersion :: Uusi
-allToAnyVersion = SetVersion "Remove all version constraints" (const True) anyVersion
+allToAnyVersion = SetVersion "All dependencies" (const True) anyVersion
 
 removeByName :: PackageName -> Uusi
 removeByName name = Remove (unPackageName name |> T.pack) (== name)
@@ -96,8 +137,8 @@ removeByName name = Remove (unPackageName name |> T.pack) (== name)
 overwriteByName :: PackageName -> VersionRange -> Uusi
 overwriteByName name = SetVersion (unPackageName name |> T.pack) (== name)
 
-replaceByName :: PackageName -> [VersionedPackage] -> Uusi
-replaceByName name = Replace (unPackageName name |> T.pack) (== name)
+replaceByName :: PackageName -> [(PackageName, VersionRange)] -> Uusi
+replaceByName name t = Replace (unPackageName name |> T.pack) (== name) (uncurry VersionedPackage <$> t)
 
 uusiCabal :: SomeUusi -> FilePath -> IO ()
 uusiCabal actions originPath = do
@@ -140,14 +181,18 @@ uusiReplace' (Replace _ p targets) x
           & myVersionRange .~ b ^. myVersionRange
     )
       <$> zip (repeat x) targets
-  | otherwise = [x]
-uusiReplace' _ x = [x]
+  | otherwise = []
+uusiReplace' _ _ = []
 
 uusiReplace :: HasVersionedPackage a => SomeUusi -> Op [a]
-uusiReplace actions t = [r | x <- t, a <- actions, r <- uusiReplace' a x]
+uusiReplace actions t
+  | k <- [r | x <- t, a <- actions, r <- uusiReplace' a x],
+    not <| null k =
+    k
+  | otherwise = t
 
 uusiRemove :: HasVersionedPackage a => SomeUusi -> Op [a]
-uusiRemove actions t = let ps = [p | a <- [actions], (Remove _ p) <- a] in filter (\x -> and <| fmap (not . (<| (x ^. myPkgName))) ps) t
+uusiRemove actions t = let ps = [p | (Remove _ p) <- actions] in filter (\x -> and <| fmap (not . (<| (x ^. myPkgName))) ps) t
 
 chain :: [Op a] -> Op a
 chain = foldr (.) id
